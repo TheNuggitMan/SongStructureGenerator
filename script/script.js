77917b6174b94fa11b033c9051cc562d549d33ef
TheNuggitMan <thenuggitman@gmail.com>
2021-03-10 17:11:11 +0000

    Created a functioning song structure return with an order!

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
 //get the output area in the index file that will produce the constructed song
 const output = document.getElementById('output');
 
-//run the output function on button press in HTML index
+//run the output function on button
 function renewOutput() {
     output.textContent = getSongStructure();
 }
 
 function getSongStructure() {
+    // used for the loop length and to retrieve a random number index in the loop
     const elementIndices = [0,1,2,3,4,5];
-    let elementOut ='';
+
+    // sets an array of 6 empty elements to be spliced into at a given position later.
+    let elementOut = [ , , , , , ];
+
+    // will be used to store each instance of the loop,
+    // then be used to test a condition with .includes,
+    // then the length after the fact will be tested against to either output or restart the loop
     let elementIndex = [];
 
     for (i = 0; i < elementIndices.length-1; i++){
@@ -21,30 +28,30 @@ function getSongStructure() {
             elementIndex.push(songElement)
             switch (songElement) {
                 case 0:
-                    elementOut += ' Intro ';
+                    elementOut.splice(0, 1, 'Intro');
                     break;
                 case 1:
-                    elementOut += ` Verse(${n}) `;
+                    elementOut.splice(1, 1, `Verse(${n})`);
                     break;
                 case 2:
-                    elementOut += ` Chorus(${n}) `;
+                    elementOut.splice(2, 1, `Chorus(${n})`);
                     break;
                 case 3:
-                    elementOut += ` Bridge(${n}) `;
+                    elementOut.splice(3, 1, `Bridge(${n})`);
                     break;
                 case 4:
-                    elementOut += ` Solo(${n}) `;
+                    elementOut.splice(4, 1, `Solo(${n})`);
                     break;
                 case 5:
-                    elementOut += ` Outro `;
+                    elementOut.splice(5, 1, `Outro`);
                     break;
                 default:
-                    elementOut += ' Work it out yourself..';
+                    elementOut.push('Work it out yourself..');
             }
-            console.log(elementIndex.length, elementIndex);
         }
+        // if output is less that 3, restart the whole loop again
         if (elementIndex.length < 3){
             return getSongStructure();
         }
-        return elementOut;
+        return elementOut.join(' ');
 };
