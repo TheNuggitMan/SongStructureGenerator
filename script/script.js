b28f7e3e840870e3dbc287d413b0c7b97769e24f
TheNuggitMan <thenuggitman@gmail.com>
2021-03-10 15:20:04 +0000

    Created a functioning song structure return with no order yet

--- script/script.js
+++ script/script.js
@@ -7,5 +7,44 @@ function renewOutput() {
 }
 
 function getSongStructure() {
-    return 'Testing --- Chorus(1), Verse(2), Outro';
-}
+    const elementIndices = [0,1,2,3,4,5];
+    let elementOut ='';
+    let elementIndex = [];
+
+    for (i = 0; i < elementIndices.length-1; i++){
+        let n = Math.ceil(Math.random()*5);
+        let songElement = elementIndices[Math.floor(Math.random()*6)];
+
+        if (elementIndex.includes(songElement)) {
+                continue;
+            }
+            elementIndex.push(songElement)
+            switch (songElement) {
+                case 0:
+                    elementOut += ' Intro ';
+                    break;
+                case 1:
+                    elementOut += ` Verse(${n}) `;
+                    break;
+                case 2:
+                    elementOut += ` Chorus(${n}) `;
+                    break;
+                case 3:
+                    elementOut += ` Bridge(${n}) `;
+                    break;
+                case 4:
+                    elementOut += ` Solo(${n}) `;
+                    break;
+                case 5:
+                    elementOut += ` Outro `;
+                    break;
+                default:
+                    elementOut += ' Work it out yourself..';
+            }
+            console.log(elementIndex.length, elementIndex);
+        }
+        if (elementIndex.length < 3){
+            return getSongStructure();
+        }
+        return elementOut;
+};
